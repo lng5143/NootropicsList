e8ce54cefbe99773c180dce2c7f7c3775bf838c6
lamnguyen <nguyenphuonglam96@gmail.com>
2024-08-22 21:48:24 +0700

    add heading data + styling

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Faq from "@/components/main/faq";
 export default function Home() {
   return (
       <div className="box-border">
-          <header className="p-10 md:p-20 text-center">
-              <h1 className="text-5xl">CURATED NOOTROPICS VENDORS</h1>
-              <p className="text-xl">Description...</p>
+          <header className="p-10 md:px-56 md:pt-20 text-center">
+              <h1 className="text-5xl mb-10 leading-snug">ELEVATE YOUR MIND WITH TRUSTED NOOTROPICS SOURCES</h1>
+              <p className="text-xl">Discover a curated selection of the finest nootropics from the most reliable vendors.</p>
           </header>
           <main className="p-10 md:p-20 flex flex-col items-center">
               <MainData />
