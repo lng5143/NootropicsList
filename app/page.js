9731c0b41c180ee9b9563c9d90edca8a7234710a
lamnguyen <nguyenphuonglam96@gmail.com>
2024-08-23 20:56:28 +0700

    update metadata + styling + delete files

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import MainData from "@/components/main/main-data";
 import Faq from "@/components/main/faq";
+import Image from "next/image";
 
 export default function Home() {
   return (
       <div className="box-border">
-          <header className="p-10 md:px-56 md:pt-20 text-center">
-              <h1 className="text-5xl mb-10 leading-snug">ELEVATE YOUR MIND WITH TRUSTED NOOTROPICS SOURCES</h1>
+          <header className="flex flex-col items-center p-10 md:px-56 md:pt-20 text-center">
+              <h1 className="text-5xl mb-2 leading-snug">ELEVATE YOUR MIND WITH TRUSTED NOOTROPICS SOURCES</h1>
+              <Image className="mb-5" src="headline-image.png" alt="chemistry" width={50} height={0} layout="intrinsic"/>
               <p className="text-xl">Discover a curated selection of the finest nootropics from the most reliable vendors.</p>
           </header>
           <main className="p-10 md:p-20 flex flex-col items-center">
